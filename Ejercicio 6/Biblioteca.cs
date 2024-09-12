a6b78a64ba593a49f6e59a9e43bd01f37d0e87d4
Martin <mujica0106@gmail.com>
2024-09-11 21:39:51 -0300

    Se termina ejercicio 6

--- Ejercicio 6/Biblioteca.cs
+++ Ejercicio 6/Biblioteca.cs
@@ -1,10 +1,53 @@
 using System.Collections;
+using System.Text;
 
 public class Biblioteca
 {
     private string libraryName;
     private ArrayList bookList = new ArrayList();
     private ArrayList membersList = new ArrayList();
-    
-    
+
+    public string LibraryName
+    {
+        get { return libraryName; }
+        set { libraryName = value; }
+    }
+
+    public Biblioteca(string name)
+    {
+        this.LibraryName = name;
+    }
+
+    public void addBook(book Book)
+    {
+        bookList.Add(Book);
+    }
+
+    public void addMember(Members member)
+    {
+        membersList.Add(member);
+    }
+
+    public void getAvaliebleBooks()
+    {
+        StringBuilder books = new StringBuilder();
+        bool avaliableBooks = false;
+
+        foreach (var b in bookList)
+        {
+            if (((book)b).Avaliable)
+            {
+                books.Append(((book)b).Title);
+            }
+        }
+
+        if (books != null)
+        {
+            Console.WriteLine($"Libros disponibles: {books} ");
+        }
+        else
+        {
+            Console.WriteLine($"No hay libros disponibles");
+        }
+    }
 }
--- obj/Debug/net8.0/RepartidoCirculo.AssemblyInfo.cs
+++ obj/Debug/net8.0/RepartidoCirculo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepartidoCirculo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+199551aa7541210cf705c105e2b76bf47308c53b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfff4bb4d185e7ddec3102b06899abda1c68c5cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepartidoCirculo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepartidoCirculo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
